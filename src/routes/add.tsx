3e379634269ae1b6f510013d2a750aafab5b5d5f
newt <ysti2019.win2020.st@gmail.com>
2024-07-22 01:39:06 +0900

    add: page navigation

--- src/routes/add.tsx
+++ src/routes/add.tsx
@@ -1,46 +1,54 @@
 import { Box, Button, Slider, TextField, Typography } from "@mui/material";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/add")({
-  component: () => (
-    <>
-      <Box px={2}>
-        <Typography variant="h5">ケガをした場所</Typography>
-      </Box>
-      <TextField
-        fullWidth
-        id="outlined-multiline-flexible"
-        label="Multiline"
-        multiline
-        rows={4}
-      />
-      <Typography variant="h5">痛みの強さ</Typography>
-      <Slider
-        aria-label="Temperature"
-        defaultValue={30}
-        marks
-        max={110}
-        min={10}
-        shiftStep={30}
-        step={10}
-        valueLabelDisplay="auto"
-      />
+  component: () => {
+    const navigate = useNavigate({ from: "/" });
+    return (
+      <>
+        <Box px={2}>
+          <Typography variant="h5">ケガをした場所</Typography>
 
-      <Typography variant="h5">痛みの深さ</Typography>
-      <Slider
-        aria-label="Temperature"
-        defaultValue={30}
-        marks
-        max={110}
-        min={10}
-        shiftStep={30}
-        step={10}
-        valueLabelDisplay="auto"
-      />
+          <TextField
+            fullWidth
+            id="outlined-multiline-flexible"
+            label="Multiline"
+            multiline
+            rows={4}
+          />
+          <Typography variant="h5">痛みの強さ</Typography>
+          <Slider
+            aria-label="Temperature"
+            defaultValue={30}
+            marks
+            max={110}
+            min={10}
+            shiftStep={30}
+            step={10}
+            valueLabelDisplay="auto"
+          />
 
-      <Button fullWidth variant="contained">
-        登録する
-      </Button>
-    </>
-  ),
+          <Typography variant="h5">痛みの深さ</Typography>
+          <Slider
+            aria-label="Temperature"
+            defaultValue={30}
+            marks
+            max={110}
+            min={10}
+            shiftStep={30}
+            step={10}
+            valueLabelDisplay="auto"
+          />
+
+          <Button
+            fullWidth
+            onClick={() => navigate({ to: "/list" })}
+            variant="contained"
+          >
+            登録する
+          </Button>
+        </Box>
+      </>
+    );
+  },
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,26 @@
 import AddIcon from "@mui/icons-material/Add";
 import { Fab, Stack } from "@mui/material";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 
 import bodyImage from "#/assets/body.jpg";
 
 export const Route = createFileRoute("/")({
-  component: () => (
-    <>
-      <Stack>
-        <img alt="aa" src={bodyImage} />
-      </Stack>
-      <Fab
-        aria-label="add"
-        color="primary"
-        sx={{ position: "fixed", bottom: "72px", right: "18px" }}
-      >
-        <AddIcon />
-      </Fab>
-    </>
-  ),
+  component: () => {
+    const navigate = useNavigate({ from: "/" });
+    return (
+      <>
+        <Stack>
+          <img alt="aa" src={bodyImage} />
+        </Stack>
+        <Fab
+          aria-label="add"
+          color="primary"
+          onClick={() => navigate({ to: "/add" })}
+          sx={{ position: "fixed", bottom: "72px", right: "18px" }}
+        >
+          <AddIcon />
+        </Fab>
+      </>
+    );
+  },
 });
